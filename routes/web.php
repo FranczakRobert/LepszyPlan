486af04675e7b1630fdf7ac53ad973150c3d927d
RobertFranczak <Franczak789@gmail.com>
2025-01-09 13:24:23 +0100

    teachers Controller - find ALL by name or surname added

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('schedules', [ScheduleController::class, 'index']);
-Route::get('teachers/{id}', [\App\Http\Controllers\TeacherController::class, 'getById']);
+Route::get('teachers/id/{id}', [\App\Http\Controllers\TeacherController::class, 'getById']);
+Route::get('teachers/name/{name}', [\App\Http\Controllers\TeacherController::class, 'getByName']);
+Route::get('teachers/surname/{surname}', [\App\Http\Controllers\TeacherController::class, 'getBySurname']);
