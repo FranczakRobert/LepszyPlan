da83f9fd3b0a6939d25cf6fe958b433a036aa6ac
RobertFranczak <Franczak789@gmail.com>
2025-01-09 22:07:13 +0100

    Implemented routing and parsing of schedule requests

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,11 @@ Route::get('rooms/id/{id}', [\App\Http\Controllers\RoomController::class, 'getBy
 Route::get('subjects/id/{id}', [\App\Http\Controllers\RoomController::class, 'getById']);
 Route::get('subjects/name/{name}', [\App\Http\Controllers\RoomController::class, 'getByName']);
 
-
-
 //TEACHERS
 Route::get('teachers/id/{id}', [\App\Http\Controllers\TeacherController::class, 'getById']);
 Route::get('teachers/name/{name}', [\App\Http\Controllers\TeacherController::class, 'getByName']);
 Route::get('teachers/surname/{surname}', [\App\Http\Controllers\TeacherController::class, 'getBySurname']);
+
+
+// REST
+Route::get('plan', [\App\Http\Controllers\RestController::class, 'generatePlan']);
